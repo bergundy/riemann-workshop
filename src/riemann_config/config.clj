96581492b2994d79390355852c037c9daef5adf8
Roey Berman <roey.berman@forter.com>
2016-08-07 14:08:42 +0300

    add system test example

--- src/riemann_config/config.clj
+++ src/riemann_config/config.clj
@@ -2,4 +2,13 @@
 
 (tcp-server {:host "127.0.0.1" :port 5555})
 (instrumentation {:enabled? false})
-(streams prn)
+(streams
+  (where (state "failed")
+    (tap :trigger-pagerduty
+      (io post))))
+
+(tests
+  (deftest state-failed-triggers-pagerduty
+    (let [event {:state "failed"}]
+      (is (= [event]
+             (:trigger-pagerduty (inject! [event])))))))
