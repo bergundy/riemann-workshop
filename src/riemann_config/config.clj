279f95249bfe6d55f50aba251b4f3292d2ac9587
Roey Berman <roey.berman@forter.com>
2016-08-08 09:33:54 +0300

    add partial solution for first exercise

--- src/riemann_config/config.clj
+++ src/riemann_config/config.clj
@@ -4,4 +4,13 @@
 (instrumentation {:enabled? false})
 (periodically-expire 2)
 
+(defn make-test-fn [pred]
+  "Returns a function that takes an event and associates
+  it's :state attribute based on given predicate"
+  (fn [e]
+    (assoc e :state (if (pred e) "passed" "failed"))))
+
+(def test-lte-0
+  (make-test-fn #(<= (:metric %) 0)))
+
 (streams prn)
