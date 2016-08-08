15480fe9a2f0e8941d356c9783dc9701601d2d60
Roey Berman <roey.berman@forter.com>
2016-08-08 09:53:25 +0300

    add partial solution for second exercise

--- src/riemann_config/config.clj
+++ src/riemann_config/config.clj
@@ -4,4 +4,18 @@
 (instrumentation {:enabled? false})
 (periodically-expire 2)
 
+(defn test-state [evs]
+  (let [last-event (last evs)
+        last-event-passed (= (:state last-event) "passed")
+        all-failed (and
+                     (= (count evs) 3)
+                     (every? #(= (:state %) "failed") evs))]
+    (assoc last-event :state
+           (if last-event-passed
+             "passed"
+             (if all-failed
+               "failed"
+               "undefined"
+             )))))
+
 (streams prn)
